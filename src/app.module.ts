8dcc3e60cb1f6107764d62d49698f67d6efc6648
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2023-11-10 15:45:34 -0300

    feat: :construction: add functionalities to category's resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { CategoriesModule } from './src/categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/common.module.ts
@@ -0,0 +1,21 @@
+import {
+  Module,
+  ValidationPipe,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { VALIDATION_PIPE_OPTIONS } from './util/common.constants';
+
+@Module({
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe(VALIDATION_PIPE_OPTIONS),
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
+})
+export class CommonModule {}
