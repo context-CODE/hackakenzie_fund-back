378d78aad29cb2e8596eb6577ae2472212b93b69
samsks <samuel.meireles.15s@outlook.com>
2023-11-13 20:14:46 -0300

    fix: :ambulance: migrations history

---
+++ prisma/migrations/20231110174638_create_category_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(20) NOT NULL,
+    "slug" VARCHAR(20) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
